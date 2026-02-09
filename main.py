da8ea351931f4a608487972a6e7ebefa7327b369
thiru061119 <151013331+thiru061119@users.noreply.github.com>
2026-02-09 22:38:36 +0530

    supabaseAPI
    API Modified from 'https://supabase.com/docs/reference/python/initializing'

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
-import psycopg2
+#import psycopg2
 from pydantic import BaseModel
 import os
+from supabase import create_client, Client
+
+url ='https://mjxobygptnklpkdktkjf.supabase.co'   #url: str = os.environ.get("SUPABASE_URL")
+key='sb_publishable_IVQGvbPJE8dbRjB6ERNodg_K2WDuTpI'   # key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
 app=FastAPI()
 #host='mjxobygptnklpkdktkjf.supabase.co',
 class User(BaseModel):
     name: str
     age: int
     #mjxobygptnklpkdktkjf
-conn=psycopg2.connect(host='db.mjxobygptnklpkdktkjf.supabase.co',database='postgres',user='postgres',password='IEq3UmvSo6sYfF5k',port=5432,sslmode="require")
-cursor = conn.cursor()
+#conn=psycopg2.connect(host='db.mjxobygptnklpkdktkjf.supabase.co',database='postgres',user='postgres',password='IEq3UmvSo6sYfF5k',port=5432,sslmode="require")
+#cursor = conn.cursor()
 
 @app.get("/test")
 def root():
@@ -18,14 +24,15 @@ def root():
 @app.post("/users")
 #def create_user(name:str,age:int):
 def create_user(user:User):
-  cursor.execute('insert into users (name,age) values (%s, %s)',(user.name,user.age))
-  conn.commit
+  response = (supabase.table("users").insert({"name": user.name, "age": user.age}).execute())  
+#  cursor.execute('insert into users (name,age) values (%s, %s)',(user.name,user.age))
+#  conn.commit
   return {'message':'User Saved'}
 @app.get("/userdata")
 def get_users():
-  cursor.execute("select name,age from users")
-  rows=cursor.fetchall()
-  return [{'name': r[0], 'age': r[1]} for r in rows]
+  #cursor.execute("select name,age from users")
+  response = (supabase.table("users").select("*").execute())    
+  return [response.json()]
   
 
 
