bea11fbdef8febb7fdc23cd2fed597c68aeec2e3
thiru061119 <151013331+thiru061119@users.noreply.github.com>
2026-02-09 20:12:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,7 @@ class User(BaseModel):
     name: str
     age: int
     #mjxobygptnklpkdktkjf
-conn=psycopg2.connect(                     
-                      host='db.mjxobygptnklpkdktkjf.supabase.co',
-                      database='postgres',
-                      user='postgres',
-                      password='IEq3UmvSo6sYfF5k',
-                      port=5432)
+conn=psycopg2.connect(host='db.mjxobygptnklpkdktkjf.supabase.co',database='postgres',user='postgres',password='IEq3UmvSo6sYfF5k',port=5432)
 cursor = conn.cursor()
 
 @app.get("/test")
